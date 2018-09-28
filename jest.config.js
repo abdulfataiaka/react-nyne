75e48dca110cd9fc8be17ccd05ba656e37a91ece
Abdulfatai Aka <abdulfataiaka@gmail.com>
2018-09-28 09:49:59 +0100

    setup coveralls and codeclimate

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   setupFiles: [
     "<rootDir>/src/jestSetup.js"
-  ]
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx}",
+    "!src/index.js",
+    "!src/helpers/*.js"
+  ],
+  coverageDirectory: "coverage"
 }
