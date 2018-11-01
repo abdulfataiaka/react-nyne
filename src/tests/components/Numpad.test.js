34feda1b594c4e33e734147d92802a107efa8f3b
abdulfataiaka <abdulfataiaka@gmail.com>
2018-11-02 00:28:39 +0100

    chore(testing): add more test
    - add tests for Numpad and Stars components to handle more edge cases
    
    [Finishes #161668577]

--- src/tests/components/Numpad.test.js
+++ src/tests/components/Numpad.test.js
@@ -19,4 +19,40 @@ describe('Numpad Component', () => {
   it('should render all 9 buttons and equal button', () => {
     expect(wrapper.find('button').length).toBe(10);
   });
+
+  it('should render exactly button 1', () => {
+    expect(wrapper.html()).toContain(1);
+  });
+
+  it('should render exactly button 2', () => {
+    expect(wrapper.html()).toContain(2);
+  });
+
+  it('should render exactly button 3', () => {
+    expect(wrapper.html()).toContain(3);
+  });
+
+  it('should render exactly button 4', () => {
+    expect(wrapper.html()).toContain(4);
+  });
+
+  it('should render exactly button 5', () => {
+    expect(wrapper.html()).toContain(5);
+  });
+
+  it('should render exactly button 6', () => {
+    expect(wrapper.html()).toContain(6);
+  });
+
+  it('should render exactly button 7', () => {
+    expect(wrapper.html()).toContain(7);
+  });
+
+  it('should render exactly button 8', () => {
+    expect(wrapper.html()).toContain(8);
+  });
+
+  it('should render exactly button 9', () => {
+    expect(wrapper.html()).toContain(9);
+  });
 });
--- src/tests/components/Stars.test.js
+++ src/tests/components/Stars.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import Stars from '../../components/Stars';
 
 describe('Stars Component', () => {
-  const wrapper = shallow(<Stars count={5} />);
+  let wrapper = shallow(<Stars count={5} />);
 
   it('should render successfully', () => {
     expect(wrapper.length).toBe(1);
@@ -11,5 +11,25 @@ describe('Stars Component', () => {
 
   it('should render 5 stars', () => {
     expect(wrapper.find('i').length).toBe(5);
+  });
+
+  it('should render 7 stars', () => {
+    wrapper = shallow(<Stars count={7} />);
+    expect(wrapper.find('i').length).toBe(7);
+  });
+
+  it('should render 1 stars', () => {
+    wrapper = shallow(<Stars count={1} />);
+    expect(wrapper.find('i').length).toBe(1);
+  });
+
+  it('should render 8 stars', () => {
+    wrapper = shallow(<Stars count={8} />);
+    expect(wrapper.find('i').length).toBe(8);
+  });
+
+  it('should render 9 stars', () => {
+    wrapper = shallow(<Stars count={9} />);
+    expect(wrapper.find('i').length).toBe(9);
   });
 });
